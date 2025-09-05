6fc8b98bc47e426cb1261030eac3fea5da38e208
janet wambua <janet2wambua@gmail.com>
2025-09-05 12:56:07 +0300

    Fix get_books_by_author function to match assignment checks

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,15 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # 1. Query all books by a specific author
+from relationship_app.models import Author, Book
+
 def get_books_by_author(author_name):
-    return Book.objects.filter(author__name=author_name)
+    # Step 1: Get the Author object
+    author = Author.objects.get(name=author_name)
+
+    # Step 2: Get all Books by that Author
+    return Book.objects.filter(author=author)
+
 
 # 2. List all books in a library
 def get_books_in_library(library_name):
