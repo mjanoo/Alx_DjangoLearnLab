a7dd6320e6f252fbf462dc186e6ddde3001017e5
janet wambua <janet2wambua@gmail.com>
2025-09-06 13:50:27 +0300

    Add book list view, template, and URLs

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,18 @@
 from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
+from django.urls import path
 from . import views  # import views from this app
 
 urlpatterns = [
