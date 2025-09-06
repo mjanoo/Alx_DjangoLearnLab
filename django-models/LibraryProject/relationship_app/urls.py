e72d49b4e0a9d60f30653a6f018d73e563b99526
janet wambua <janet2wambua@gmail.com>
2025-09-06 18:08:20 +0300

    Added authentication URLs and register view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,21 +1,17 @@
 from django.urls import path
-from .views import (
-    list_books,
-    LibraryDetailView,
-    register_view,
-    login_view,
-    logout_view,
-)
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     # Function-Based View: list all books
-    path("books/", list_books, name="list_books"),
+    path("books/", views.list_books, name="list_books"),
 
     # Class-Based View: show library details
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 
-    # Authentication Views
-    path("register/", register_view, name="register"),
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
+    # User Authentication
+    path("register/", views.register, name="register"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 ]
+
