58b835fb4d4f35ab7b014ee63a95196bfa26100a
janet wambua <janet2wambua@gmail.com>
2025-09-07 20:01:57 +0300

    Add role-based access control with admin, librarian, and member views using @user_passes_test

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
-from .models import Book, Library, UserProfile
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Book, Library, UserProfile
+
 
 # ----------------------------
 # Book Views
@@ -31,7 +32,7 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return render(request, "relationship_app/list_books.html")
+            return redirect("list_books")
     else:
         form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
@@ -43,7 +44,7 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return render(request, "relationship_app/list_books.html")
+            return redirect("list_books")
     else:
         form = AuthenticationForm()
     return render(request, "relationship_app/login.html", {"form": form})
@@ -60,8 +61,10 @@ def logout_view(request):
 def is_admin(user):
     return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
 
+
 def is_librarian(user):
     return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
 
 def is_member(user):
     return hasattr(user, "userprofile") and user.userprofile.role == "Member"
@@ -71,18 +74,19 @@ def is_member(user):
 # Role-Based Views
 # ----------------------------
 @login_required
-@user_passes_test(is_admin, login_url="login")
+@user_passes_test(is_admin, login_url='/login/')
 def admin_view(request):
     return render(request, "relationship_app/admin_view.html")
 
 
 @login_required
-@user_passes_test(is_librarian, login_url="login")
+@user_passes_test(is_librarian, login_url='/login/')
 def librarian_view(request):
     return render(request, "relationship_app/librarian_view.html")
 
 
 @login_required
-@user_passes_test(is_member, login_url="login")
+@user_passes_test(is_member, login_url='/login/')
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
+
