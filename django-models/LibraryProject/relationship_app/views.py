20bb5a0a3a0ab2eecae71bb10ec399736bf3ecc4
janet wambua <janet2wambua@gmail.com>
2025-09-07 19:56:02 +0300

    Fix role-based access views with @user_passes_test and login_url

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -71,18 +71,18 @@ def is_member(user):
 # Role-Based Views
 # ----------------------------
 @login_required
-@user_passes_test(is_admin)
+@user_passes_test(is_admin, login_url="login")
 def admin_view(request):
     return render(request, "relationship_app/admin_view.html")
 
 
 @login_required
-@user_passes_test(is_librarian)
+@user_passes_test(is_librarian, login_url="login")
 def librarian_view(request):
     return render(request, "relationship_app/librarian_view.html")
 
 
 @login_required
-@user_passes_test(is_member)
+@user_passes_test(is_member, login_url="login")
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
